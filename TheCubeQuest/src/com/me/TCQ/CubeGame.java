1ba492305570d455e6109048421435eabf5cbdf7
ivanmartinezperez <ivan@192.168.1.42>
2014-01-26 20:22:27 +0100

    Declaración de Screen añadida

--- TheCubeQuest/src/com/me/TCQ/CubeGame.java
+++ TheCubeQuest/src/com/me/TCQ/CubeGame.java
@@ -3,6 +3,7 @@ package com.me.TCQ;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -16,6 +17,7 @@ public class CubeGame extends Game {
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	private Screen mainmenu;
 	
 	@Override
 	public void create() {		
