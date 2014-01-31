c251f244c3704883440c7800dfcaf21bbaa90ced
ivanmartinezperez <ivan@192.168.0.199>
2014-01-31 20:02:50 +0100

    GroupTemplate added

---
+++ TheCubeQuest/src/com/me/TCQ/LevelWorld/GroupTemplate.java
@@ -0,0 +1,11 @@
+package com.me.TCQ.LevelWorld;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+public class GroupTemplate extends Group {
+
+	public GroupTemplate() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
