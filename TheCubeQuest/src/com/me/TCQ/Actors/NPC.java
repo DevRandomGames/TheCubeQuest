e2d8e0ecb1516bff3aecf404cfe67fc6b0fab475
ivanmartinezperez <ivan@192.168.1.39>
2014-02-06 00:54:37 +0100

    State moved to NPC Class, more sense

--- TheCubeQuest/src/com/me/TCQ/Actors/NPC.java
+++ TheCubeQuest/src/com/me/TCQ/Actors/NPC.java
@@ -1,6 +1,12 @@
 package com.me.TCQ.Actors;
 
 public abstract class NPC extends Entity {
+	
+	public enum State {
+		IDLE,ALERT,
+		HOSTILE,ALIES
+	}
+
 
 	public NPC() {
 		// TODO Auto-generated constructor stub
