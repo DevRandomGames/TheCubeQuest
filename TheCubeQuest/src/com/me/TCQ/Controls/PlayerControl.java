54e3434aca15888dfd90e425a5a5bed300b450c7
ivanmartinezperez <ivan@192.168.1.39>
2014-02-06 23:09:48 +0100

    Basic Player Control (Errors because player no implemeted yet...Don't panic)

--- TheCubeQuest/src/com/me/TCQ/Controls/PlayerControl.java
+++ TheCubeQuest/src/com/me/TCQ/Controls/PlayerControl.java
@@ -1,9 +1,76 @@
 package com.me.TCQ.Controls;
 
-public class PlayerControl extends ControlTemplate {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.me.TCQ.Actors.PlayerEntity;
 
-	public PlayerControl() {
-		// TODO Auto-generated constructor stub
+public class PlayerControl extends ControlTemplate {
+	
+	PlayerEntity character;
+
+	public PlayerControl(PlayerEntity p) {
+		
 	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		switch(keycode){
+			case Input.Keys.W: character.Jump();break;
+			case Input.Keys.A: character.WalkLeft();break;
+			case Input.Keys.S: character.Down();break;
+			case Input.Keys.D: character.WalkRight();break;
+			case Input.Keys.E: character.Interaction();break;
+				
+		}
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return super.keyUp(keycode);
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return super.keyTyped(character);
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		switch(button){
+			case Input.Buttons.LEFT: character.Attack();break;
+			case Input.Buttons.RIGHT: character.DefenseOn();break;
+			
+		}
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		switch(button){
+			case Input.Buttons.RIGHT: character.DefenseOff();break;
+		}
+		
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return super.touchDragged(screenX, screenY, pointer);
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return super.mouseMoved(screenX, screenY);
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return super.scrolled(amount);
+	}
+	
+	
 
 }
