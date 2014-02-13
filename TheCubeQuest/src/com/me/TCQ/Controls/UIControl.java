cd61778811db746b8b1f3f4bd70db29a6495e4a9
Ivan <Ivan@Ivan-PC>
2014-02-13 16:28:01 +0100

    Uicontrol class started

--- TheCubeQuest/src/com/me/TCQ/Controls/UIControl.java
+++ TheCubeQuest/src/com/me/TCQ/Controls/UIControl.java
@@ -1,12 +1,64 @@
 package com.me.TCQ.Controls;
 
+import com.me.TCQ.UserInterface.ScreenTemplate;
+
 public class UIControl extends ControlTemplate {
 	
-	
+	ScreenTemplate screen;
 		
-	public UIControl() {
-		// TODO Auto-generated constructor stub
+	public UIControl(ScreenTemplate Screen) {
+		screen = Screen;
 	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return super.keyDown(keycode);
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return super.keyUp(keycode);
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return super.keyTyped(character);
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return super.touchDown(screenX, screenY, pointer, button);
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return super.touchUp(screenX, screenY, pointer, button);
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return super.touchDragged(screenX, screenY, pointer);
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return super.mouseMoved(screenX, screenY);
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return super.scrolled(amount);
+	}
+	
+	
 	
 
 }
